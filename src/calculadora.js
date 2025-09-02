c6f81b72b6550817ca6d143f9d0b61cdcad7ffa1
AnaWeiz <ana_laura_w@hotmail.com>
2025-09-02 00:43:09 -0300

    Adicionei a soma e o seu teste

---
+++ src/calculadora.js
@@ -0,0 +1,17 @@
+function somarDoisNumeros(valor1, valor2){
+    const resultado = valor1 + valor2;
+    return resultado;
+}
+
+function calcularMediaDeDoisNumeros(valor1, valor2){
+    //1. Soma todos os valores
+    const resultadoSomaDeDoisValores = somarDoisNumeros(valor1, valor2);
+    //2. Divide pela quantiadade de números passado
+    const resultadoDaMediaDeDoisValores = resultadoSomaDeDoisValores / 2;
+    //3. Retornar o resultado
+    return resultadoDaMediaDeDoisValores;
+}
+
+module.exports = {
+    somarDoisNumeros  //coloco as funções que quero exportar para outro file (uma abaixo da outra)
+}
---
+++ tests/calculadora.test.js
@@ -0,0 +1,5 @@
+const { somarDoisNumeros } = require('../src/calculadora'); //sair da pasta tests, entrar na pasta src e pegar 'calculadora'
+
+const resultadoDaSoma = somarDoisNumeros(5, 3);
+
+console.log(resultadoDaSoma);
